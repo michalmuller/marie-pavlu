fee01a0d4f2f159a0920978b0ec4e3eadd458434
Michal Muller <michal@towoo.dk>
2019-07-11 19:49:21 +0200

    dist

---
+++ dist/precache-manifest.bfdf17985dfe77642652422cbfc1e9ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f218b79237958d5b6a90671213cecb21",
+    "url": "/img/Group 50.f218b792.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "dfd246b415b6095cced5",
+    "url": "/js/chunk-vendors.b4424b0c.js"
+  },
+  {
+    "revision": "be75bbb8b8a230f1bf69",
+    "url": "/js/app.1bb3bf39.js"
+  },
+  {
+    "revision": "fa48bcf95db7e4ce30f4e13d4743b471",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d756ed735c963d68f0d984aaa767de37",
+    "url": "/img/user.d756ed73.svg"
+  },
+  {
+    "revision": "a666df2fba2120b084e2788edb7c6d40",
+    "url": "/img/fda_icon.a666df2f.svg"
+  },
+  {
+    "revision": "fe331c690a8f481cc528ed5f2d1b9805",
+    "url": "/img/download.fe331c69.svg"
+  },
+  {
+    "revision": "f218b79237958d5b6a90671213cecb21",
+    "url": "/img/Group 50.png"
+  },
+  {
+    "revision": "dfd246b415b6095cced5",
+    "url": "/css/chunk-vendors.854acce8.css"
+  },
+  {
+    "revision": "be75bbb8b8a230f1bf69",
+    "url": "/css/app.c20196d9.css"
+  }
+];
