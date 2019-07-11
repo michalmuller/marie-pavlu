694b29af91f451f30e4773db3f72cf73413c00d3
Michal Muller <michal@towoo.dk>
2019-07-11 11:10:34 +0200

    testimonial card in progress

--- src/components/TestimonialsSection.vue
+++ src/components/TestimonialsSection.vue
@@ -1,13 +1,38 @@
 <template>
   <div class="bg-gradient">
     <div class="container flex flex-wrap justify-between flex-row py-16">
-      <h1 class="font-bold text-3xl text-white">Co o nas rikaji nasi pacienti</h1>
+      <h1 class="w-full font-bold text-3xl text-white pb-12">Co o nas rikaji nasi pacienti</h1>
+      <testimonial v-for="(person, index) in people" :key="index" :person="person"></testimonial>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Testimonial from "./common/Testimonial";
+export default {
+  components: { Testimonial },
+  data() {
+    return {
+      people: [
+        {
+          name: "Roman Muller",
+          img: require("../../public/img/appointment.png"),
+          cardMargin: "mr-6"
+        },
+        {
+          name: "Roman Muller",
+          img: require("../../public/img/appointment.png"),
+          cardMargin: "mx-3"
+        },
+        {
+          name: "Roman Muller",
+          img: require("../../public/img/appointment.png"),
+          cardMargin: "ml-6"
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/common/Testimonial.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="w-1/3">
+    <div class="rounded bg-white shadow-md px-10 py-10" :class="person.cardMargin">
+      <div class="w-full flex justify-center">
+        <img class="w-20 h-20 rounded-full" :src="person.img" alt="Sunset in the mountains" />
+      </div>
+      <p class="w-full flex justify-center pt-4 font-bold text-xl mb-6">{{person.name}}</p>
+      <div>
+        <img
+          class="icon-quote w-10 pt-1 mr-4 float-left"
+          src="../../../public/img/icons/right-quote-sign.svg"
+          alt
+        />
+        <p
+          class="leading-normal text-secondary-text"
+        >Lorem ipsum dolor sit amet, velit. Donec varius vehicula magna sit amet auctor. Vivamus suscipit eleifend turpis, nec sodales sem vulputate a. Curabitur pulvinar libero viverra, efficitur odio eu, finibus justo. Etiam eu vehicula felis.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["person"]
+};
+</script>
+
+<style lang="scss" scoped>
+.icon-quote {
+}
+</style>
