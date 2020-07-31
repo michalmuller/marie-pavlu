9388579acb7f5ab7418616e7a0174861d6da94a2
Michal Muller <michal@towoo.dk>
2020-07-31 17:11:36 +0200

    vacation announcement

--- src/components/announcementsSection.vue
+++ src/components/announcementsSection.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="announcements" class="bg-white">
-    <!-- <div class="container flex flex-wrap justify-between flex-row pt-16 px-4 sm:px-0">
+    <div class="container flex flex-wrap justify-between flex-row pt-16 px-4 sm:px-0">
       <div class="w-full">
-        <h1 class="font-bold text-red text-3xl underline">OPATŘENÍ PROTI ŠÍŘENÍ KORONAVIRU !</h1>
-        <ul class="list-decimal pl-4">
+        <h1 class="font-bold text-red text-center text-3xl md:text-4xl">DOVOLENÁ 3.8 - 14.8</h1>
+        <!-- <ul class="list-decimal pl-4">
           <li
             class="text-secondary-text pt-4"
           >Všichni prosím omezte pobyt mimo domov na nezbytné mimimum.</li>
@@ -23,19 +23,38 @@
           <li
             class="pt-4 text-secondary-text"
           >Vzhledem k plošnému nedostatku ochranných pomůcek je možné, že dojde k dočasnému omezení provozu ordinace. Sledujte prosím aktuální informace na dveřích naší ordinace nebo na webových stránkách</li>
-        </ul>
-    </div>
-    <div class="bg-light-blue w-full pt-6 pb-8 mt-8 rounded text-center">
-        <h2
-          class="text-2xl md:text-3xl font-bold md:font-semibold px-6"
-        >Ošetření pacientů pouze po telefonickém objednání</h2>
-        <p
-          style="margin:0 auto"
-          class="pt-2 px-6 max-w-3xl text-secondary-text flex align-center"
-        >Dovolená 1.7 - 3.8.2020 - zástup MUDr. Olga Svobodová, Dolní náměstí. (pouze akutní případy) Požadavky na léky ev. formou sms na tel: 601 538 013 &#128522</p>
-        <p class="pt-4 font-bold text-2xl">Děkujeme !</p>
+        </ul>-->
       </div>
-    </div>-->
+      <div class="bg-light-blue w-full pt-6 pb-8 mt-8 rounded-lg text-center">
+        <h2
+          class="text-2xl md:text-3xl font-bold md:font-semibold underline px-6"
+        >Zástup 3.8 - 7.8 MUDr. Barbora Škývarová</h2>
+
+        <p
+          class="pt-6 font-semibold text-lg px-4 text-secondary-text"
+        >tel: 483 341 217 | 3 patro poliklinika Po-Pá 8-10, Čt 13-15</p>
+        <p
+          class="pt-2 font-semibold text-lg px-4 text-secondary-text"
+        >Pouze akutní případy - po telefonické domluvě</p>
+      </div>
+
+      <div class="bg-light-blue w-full pt-6 pb-8 mt-8 rounded-lg text-center">
+        <h2
+          class="text-2xl md:text-3xl font-bold md:font-semibold underline px-6"
+        >Zástup 10.8 - 14.8 MUDr. Karel Stuchlík</h2>
+
+        <p
+          class="pt-6 font-semibold text-lg px-4 text-secondary-text"
+        >tel: 602 191 976 | Ordinace Josefův Důl Po-Pá 8-10, St 13-15</p>
+        <p
+          class="pt-2 font-semibold text-lg px-4 text-secondary-text"
+        >Pouze akutní případy - po telefonické domluvě</p>
+      </div>
+      <p
+        style="margin: 0 auto;"
+        class="pt-8 font-semibold text-2xl text-center"
+      >Požadavky na léky ev. formou sms na tel: 601 538 013</p>
+    </div>
   </div>
 </template>
 
