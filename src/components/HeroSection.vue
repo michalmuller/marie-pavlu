79b1bbdd7e850bbf16b23a6347419b507f6df8f3
Michal Muller <michalll.muller@gmail.com>
2024-08-05 18:31:51 +0200

    test

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -6,24 +6,30 @@
         <div
           class="lg:w-1/2 md:w-2/3 sm:w-2/3 w-4/5 mt-12 px-10 py-8 introduction rounded-lg rounded-r-none"
         >
-          <h1 class="text-3xl font-bold">MUDr. Marie Pavlů</h1>
-          <h2 class="text-xl font-semibold mb-4">Praktický lékař pro dospělé</h2>
+          <h1 class="text-3xl font-bold">MUDr. Mariie Pavlů</h1>
+          <h2 class="text-xl font-semibold mb-4">
+            Praktický lékař pro dospělé
+          </h2>
           <div class="md:w-5/6 lg:w-3/5 text-secondary-text">
             <a
               class="block"
               target="_blank"
               href="https://www.google.com/maps/place/Po%C5%A1tovn%C3%AD+2428%2F8,+466+01+Jablonec+nad+Nisou,+Czechia/@50.7237417,15.163313,17z/data=!3m1!4b1!4m5!3m4!1s0x470ecad2748dda93:0xf10ed0a00582c2bc!8m2!3d50.7237383!4d15.1655017"
-            >Jablonec nad Nisou, Poliklinika I, Postovni 2428.</a>
+              >Jablonec nad Nisou, Poliklinika I, Postovni 2428.</a
+            >
 
             <a
               class="block mt-1"
               target="_blank"
               href="https://www.google.com/maps/place/Janov+nad+Nisou+514,+468+11+Janov+nad+Nisou,+Czechia/@50.7729918,15.1655906,17z/data=!3m1!4b1!4m5!3m4!1s0x470934a6f0a7ecc7:0x206a5349c69adfd6!8m2!3d50.7729884!4d15.1677793"
-            >Janov nad Nisou, 514.</a>
+              >Janov nad Nisou, 514.</a
+            >
           </div>
-          <h3 class="font-bold mt-10 md:w-5/6 lg:w-3/5 text-lg" style="max-width:370px">
-            V březnu 2019 jsme převzali pacienty od
-            MUDr. Milana Kvasničky
+          <h3
+            class="font-bold mt-10 md:w-5/6 lg:w-3/5 text-lg"
+            style="max-width: 370px"
+          >
+            V březnu 2019 jsme převzali pacienty od MUDr. Milana Kvasničky
           </h3>
           <news class="mt-6">
             <p class="px-3">nově smlouvy s pojišťovnou 201 (vojenská + OZP)</p>
@@ -68,7 +74,7 @@
 import Card from "./common/Card";
 import News from "./common/News";
 export default {
-  components: { Card, News }
+  components: { Card, News },
 };
 </script>
 
